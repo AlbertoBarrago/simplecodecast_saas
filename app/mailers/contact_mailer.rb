c9ae6d320a0a0a087ed3628c5f1dc7f6f360f2ff
Albz <studio@albertobarrago.it>
2015-08-08 18:29:31 +0000

    Fixed problem send Email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
     default to: 'a.barrago69@gmail.com'
     
-    def contact_email(name,email,body)
+    def contact_mail(name,email,body)
         @name = name
         @email = email
         @body = body 
