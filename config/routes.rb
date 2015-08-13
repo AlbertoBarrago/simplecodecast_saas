d960580ca642e382bac86e89704f5d42c73a7b1e
Albz <studio@albertobarrago.it>
2015-08-13 17:05:52 +0000

    Added controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :user do 
+    resource :profile
+  end
   resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
