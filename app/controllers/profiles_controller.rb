3b6bc1ef269c383385b87f212b8d73368f4e2206
Albz <studio@albertobarrago.it>
2015-08-16 16:38:36 +0000

    Addedo contact form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,23 @@
 class ProfilesController < ApplicationController
-    def new 
-      # form where a user can fill out theri own profile.
-      @user = User.find( params[:user_id] )
-      @variable = params[:name]
-      @profile = @user.build_profile
+  def new
+    # form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
+  end
+  
+  def create 
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
+    end
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
 end
