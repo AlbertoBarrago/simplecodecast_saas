382e4f04f9bbf8cd6013bf4b01290f0c9463e5d0
Albz <studio@albertobarrago.it>
2015-08-19 17:14:08 +0000

    Added imageMagick and improve general enviroment, now all works

--- Gemfile
+++ Gemfile
@@ -42,8 +42,7 @@ gem 'devise','3.4.1'
 gem 'stripe', '1.16.1'
 gem 'figaro', '1.0.0'
 
-#Use paperclip for image upload
-gem 'paperclip'
+gem 'paperclip', '4.2.1'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819161547) do
+ActiveRecord::Schema.define(version: 20150819165619) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -59,6 +59,10 @@ ActiveRecord::Schema.define(version: 20150819161547) do
     t.datetime "updated_at"
     t.integer  "plan_id"
     t.string   "stripe_customer_token"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
